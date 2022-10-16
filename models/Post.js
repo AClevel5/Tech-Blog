ee36bf5cda4126ecda759381ae98ab5cf988caf9
Alex Cleveland <Alexcleveland5@gmail.com>
2022-10-16 14:32:52 -0600

    Post model edit

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,32 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
-class Post extends Model {}
+class Post extends Model { }
 
 Post.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
+
   {
-    sequelize
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Post"
+
   }
 );
 
