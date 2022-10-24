760c5f3f31c6109b4d9e795540ca59716df8e7bc
Alex Cleveland <Alexcleveland5@gmail.com>
2022-10-23 18:03:12 -0600

    Working now fixed models

--- models/Comment.js
+++ models/Comment.js
@@ -5,41 +5,54 @@ const Post = require('./Post');
 
 class Comment extends Model { }
 
+// Comment.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     body: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: User,
+//         key: 'id'
+//       }
+//     },
+//     postId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: Post,
+//         key: 'id'
+//       }
+//     }
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'Comment'
+//   }
+// );
+
 Comment.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     body: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: 'id'
-      }
-    },
-    postId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: Post,
-        key: 'id'
-      }
     }
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'Comment'
+    sequelize
+
   }
 );
 
--- models/Post.js
+++ models/Post.js
@@ -4,38 +4,52 @@ const User = require('./User')
 
 class Post extends Model { }
 
+// Post.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     title: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     body: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: User,
+//         key: 'id',
+//       }
+//     }
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "Post"
+
+//   }
+// );
+
 Post.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: User,
-        key: 'id',
-      }
-    }
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "Post"
-
+    sequelize
   }
 );
+
+
+
+
 
 module.exports = Post;
